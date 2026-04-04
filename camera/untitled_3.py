895f772622add3afd0ba76c848090d5bcf956bea
kseniiape <165958110+kseniiape@users.noreply.github.com>
2026-04-04 21:47:33 +0300

    Add files via upload

--- camera/untitled_3.py
+++ camera/untitled_3.py
@@ -1,31 +1,32 @@
 
 import sensor, image, time, math, pyb
 
-robot = 1 #2 - g, 1 - f
+robot =  2#2 - g, 1 - f
 
 
 if robot == 1:
 
     EXPOSURE_TIME_SCALE = 0.4
-    yellow_threshold = [(0, 100, -128, 127, 30, 127)]
-    blue_threshold = [(0, 100, -128, -7, -128, -2)]
-    exposure = 40_000
-    gain = 25
-    center  = [165,122]
-    white = (63, 60, 63)
-
+    yellow_threshold = [(0, 100, -128, 127, 26, 127)]
+    blue_threshold = [(0, 100, -128, -7, -128, -4)]
+    red_threshold = [(0, 100, 16, 75, 18, 44)]
+    exposure = 140_000
+    gain = 20
+    center  = [167,110]
+    white = (63, 60, 62)
 
 else:
 
     EXPOSURE_TIME_SCALE = 0.4
 
     EXPOSURE_TIME_SCALE = 0.4
-    yellow_threshold = [(0, 100, -128, 127, 20, 127)]
-    blue_threshold = [(0, 100, -128, 127, -128, -18)]
-    exposure = 90_000
+    yellow_threshold = [(0, 100, -128, 127, 26, 127)]
+    blue_threshold = [(0, 100, -128, -7, -128, -4)]
+    red_threshold = [(0, 100, 9, 30, 14, 24)]
+    exposure = 140_000
     gain = 20
-    center  = [165,111]
-    white = (64, 60, 63)
+    center  = [166,125]
+    white = (63, 60, 62)
 
 
 img_radius = 135
@@ -33,7 +34,7 @@ red_led = pyb.LED(2)
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QVGA)
+sensor.set_framesize(sensor.QQVGA)
 sensor.set_auto_gain(True)
 sensor.set_auto_whitebal(True)
 
@@ -44,6 +45,7 @@ sensor.set_auto_exposure(True, \
     exposure_us = int(current_exposure_time_in_microseconds* EXPOSURE_TIME_SCALE))
 clock = time.clock()
 sensor.skip_frames(time = 500)
+print(sensor.get_rgb_gain_db())
 
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
@@ -53,7 +55,7 @@ sensor.set_auto_whitebal(False)
 sensor.set_auto_exposure(False)
 current_exposure_time_in_microseconds =  sensor.get_exposure_us()
 sensor.set_auto_exposure(False, exposure_us = exposure)
-clock = time.clock()
+#clock = time.clock()
 sensor.skip_frames(time = 1000)
 
 uart = pyb.UART(3, 115200, timeout = 100, timeout_char = 100)
@@ -61,6 +63,13 @@ uart.init(115200, bits=8, parity=False, stop=1, timeout_char=100)
 
 image_height = sensor.height()
 image_width = sensor.width()
+
+ball_roundness = 0
+
+my_blob_x = 0
+my_blob_y = 0
+ball_angle = 0
+ball_distance = 0
 
 old_area = 0
 yellow_angle = 0
@@ -120,7 +129,7 @@ def crc8(data, len): #function that calculates check sum
 
 data = bytearray(7)
 
-def send_data (num1, num2, num3, num4):
+def send_data (num1, num2, num3, num4, num5, num6):
     uart.writechar(255)
     red_led.on()
     # num1 = int((num1) / 3) + 127
@@ -131,6 +140,8 @@ def send_data (num1, num2, num3, num4):
     num2 = int((num2) / 2)
     num3 = int((num3) / 3)
     num4 = int((num4) / 2)
+    num5 = int((num5) / 3)
+    num6 = int((num6) / 2)
 
     if num1 + 127 > 253:
        data[0] = 253
@@ -160,9 +171,20 @@ def send_data (num1, num2, num3, num4):
     else:
         data[3] = num4
 
+    if num5 + 127 > 253:
+       data[4] = 253
+    elif num5 + 127 < 0:
+       data[4] = 0
+    else:
+       data[4] = num5
 
-
-    data[4] = crc8(data, 4)
+    if num6 + 127 > 253:
+       data[5] = 253
+    elif num6 + 127 < 0:
+       data[5] = 0
+    else:
+       data[5] = num6
+    data[6] = crc8(data, 6)
 
 
     uart.writechar(int(data[0]))
@@ -170,6 +192,8 @@ def send_data (num1, num2, num3, num4):
     uart.writechar(int(data[2]))
     uart.writechar(int(data[3]))
     uart.writechar(int(data[4]))
+    uart.writechar(int(data[5]))
+    uart.writechar(int(data[6]))
     red_led.off()
 
 def linearize(num):
@@ -222,10 +246,9 @@ while True:
     clock.tick()
     img = sensor.snapshot().mask_circle(center[0], center[1], img_radius)
     old_area = 0
-
     img.draw_circle(center[0], center[1], 3, (0, 0, 0), fill = True)
 
-    for blob in img.find_blobs(yellow_threshold, pixels_threshold=48, area_threshold=195, merge=True, margin = 20):
+    for blob in img.find_blobs(yellow_threshold, pixels_threshold=30, area_threshold=300, merge=True, margin = 20):
         if(blob[2] * blob[3] > old_area and blob[2] * blob[3] > 60):
             old_area = blob[2] * blob[3]
             yellow = [blob.x(), blob.y(), blob.x()+blob.w(),blob.y()+blob.h(), blob.cx(), blob.cy(), blob.w(), blob.h()]
@@ -248,7 +271,7 @@ while True:
 
     old_area = 0
 
-    for blob in img.find_blobs(blue_threshold, pixels_threshold=48, area_threshold=195, merge=True, margin = 20):
+    for blob in img.find_blobs(blue_threshold, pixels_threshold=30, area_threshold=300, merge=True, margin = 20):
         if(blob[2] * blob[3] > old_area and blob[2] * blob[3] > 60):
             old_area = blob[2] * blob[3]
             blue = [blob.x(), blob.y(), blob.x()+blob.w(),blob.y()+blob.h(), blob.cx(), blob.cy(), blob.w(), blob.h()]
@@ -264,15 +287,44 @@ while True:
     if(old_area < 50):
         blue_distance = 0
     #print("blue_distance =", blue_distance)
-
     #print("blue_angle =", blue_angle)
-
     if(blue_angle > 360):
         blue_angle -= 360
     elif(blue_angle < 0):
         blue_angle += 360
 
     old_area = 0
+    old_roundness = 0
+    max_area = 0
+    ball_distance = 0
+    for blob in img.find_blobs(red_threshold, pixels_threshold=0, area_threshold=0, merge=True, margin = 10):
+        if blob.area() >= 7 and blob.area() < 350:
+            if blob.compactness() > old_roundness:
+                old_roundness = blob.compactness()
+                my_blob = blob
+                max_area = blob.area()
+                my_blob_x = blob.cx()
+                my_blob_y = blob.cy()
+                ball_x = (my_blob_x - center[0])
+                ball_y = (my_blob_y - center[1])
+                ball_distance = linearize(get_distance(ball_x, ball_y))
+                ball_angle = math.floor(math.atan2(ball_x, ball_y) * 57.3) + 90
+                img.draw_circle(my_blob_x, my_blob_y, 20, (255, 255, 255), 4)
+    #print(max_area)
 
-    send_data(yellow_angle, yellow_distance, blue_angle, blue_distance)
+    #print("ball_distance =", ball_distance)
+    #print("ball_angle =", ball_angle)
+    #img.draw_line(yellow[4], yellow[5], blue[4], blue[5], (255, 255, 255), 2) #line between centers of gates
+
+
+    if(ball_angle > 360):
+        ball_angle -= 360
+    elif(ball_angle < 0):
+        ball_angle += 360
+
+
+
+
+    #send_data(yellow_angle, yellow_distance, blue_angle, blue_distance)
+    send_data(yellow_angle, yellow_distance, blue_angle, blue_distance, ball_angle, ball_distance)
 
