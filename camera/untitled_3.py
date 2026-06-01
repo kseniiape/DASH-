816c0254533baeb9f5b8702a5fcd42239083c5aa
kseniiape <165958110+kseniiape@users.noreply.github.com>
2026-06-01 19:37:05 +0300

    Обнаружение мяча по камере

--- camera/untitled_3.py
+++ camera/untitled_3.py
@@ -1,36 +1,38 @@
 
 import sensor, image, time, math, pyb
 
-robot =  2#2 - g, 1 - f
+robot =2#2 - g, 1 - f
 
 
 if robot == 1:
 
     EXPOSURE_TIME_SCALE = 0.4
-    yellow_threshold = [(0, 100, -128, 127, 26, 127)]
-    blue_threshold = [(0, 100, -128, -7, -128, -4)]
-    red_threshold = [(0, 100, 16, 75, 18, 44)]
-    exposure = 140_000
-    gain = 20
+    yellow_threshold = [(0, 100, 14, 32, 32, 127)]
+    blue_threshold = [(0, 100, -128, -5, -128, -1)]
+    # red_threshold = [(39, 56, 26, 64, 24, 49)]
+    red_threshold = [(0, 100, 35, 58, 22, 32)]
+    exposure = 80_000
+    gain  = 17
     center  = [167,110]
-    white = (63, 60, 62)
+    white = (63, 60, 63)
 
 else:
 
     EXPOSURE_TIME_SCALE = 0.4
 
     EXPOSURE_TIME_SCALE = 0.4
-    yellow_threshold = [(0, 100, -128, 127, 26, 127)]
-    blue_threshold = [(0, 100, -128, -7, -128, -4)]
-    red_threshold = [(0, 100, 9, 30, 14, 24)]
-    exposure = 140_000
-    gain = 20
-    center  = [166,125]
-    white = (63, 60, 62)
-
+    yellow_threshold = [(0, 100, -128, 127, 40, 127)]
+    blue_threshold = [(0, 100, -128, 0, -128, -6)]
+    red_threshold = [(0, 100, 34, 127, 29, 40)]
+    exposure = 80_000
+    gain = 17
+    center  = [168,122]
+    white = (63, 60, 63)
 
 img_radius = 135
 red_led = pyb.LED(2)
+
+
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -52,7 +54,7 @@ sensor.set_framesize(sensor.QVGA)
 sensor.set_auto_gain(False, gain_db=gain)
 sensor.set_auto_whitebal(False, rgb_gain_db = white)
 sensor.set_auto_whitebal(False)
-sensor.set_auto_exposure(False)
+#sensor.set_auto_exposure(False)
 current_exposure_time_in_microseconds =  sensor.get_exposure_us()
 sensor.set_auto_exposure(False, exposure_us = exposure)
 #clock = time.clock()
@@ -78,37 +80,71 @@ yellow_distance = 0
 blue_distance = 0
 
 distance = [
-            [210, 123],
-            [175, 121],
+            [210, 127],
+            [175, 122],
+            [165, 121],
+            [155, 118],
+            [145, 117],
+            [140, 116],
+            [135, 115],
+            [130, 114],
+            [125, 112],
+            [115, 111],
+            [110, 109],
+            [105, 108],
+            [100, 106],
+            [95, 103],
+            [90, 101],
+            [85, 100],
+            [80, 97],
+            [75, 94],
+            [70, 89],
+            [65, 85],
+            [60, 82],
+            [55, 78],
+            [50, 73],
+            [45, 67],
+            [40, 61],
+            [35, 54],
+            [30, 47],
+            [25, 41],
+            [20, 37],
+            [15, 36],
+            [10, 25],
+                    ]
+
+distance_ll = [
+            [210, 125],
+            [175, 122],
             [165, 120],
             [155, 118],
             [145, 116],
             [140, 115],
-            [135, 113],
+            [135, 114],
+            [130, 112],
             [125, 111],
-            [120, 110],
-            [115, 108],
-            [110, 105],
-            [105, 103],
-            [100, 101],
-            [95, 98],
-            [90, 96],
+            [115, 107],
+            [110, 106],
+            [105, 105],
+            [100, 102],
+            [95, 100],
+            [90, 97],
             [85, 94],
-            [80, 92],
-            [75, 89],
-            [70, 83],
-            [65, 79],
-            [60, 74],
-            [55, 71],
-            [50, 65],
-            [45, 59],
-            [40, 52],
-            [35, 45],
-            [30, 38],
-            [25, 33],
-            [20, 31],
-            [15, 28],
-            [10, 25],
+            [80, 91],
+            [75, 88],
+            [70, 84],
+            [65, 80],
+            [60, 73],
+            [55, 67],
+            [50, 61],
+            [45, 53],
+            [40, 44],
+            [35, 36],
+            [30, 26],
+            [25, 23],
+            [20, 18],
+            [15, 10],
+            [10, 5],
                     ]
 
 def get_distance(x, y):
@@ -240,6 +276,51 @@ def linearize(num):
         result = distance[i][0]
     return result
 
+def linearize_ll(num):
+    num = abs(num)
+    i = -1
+    old_err = 1000
+    for mas in distance_ll:
+        err = num - abs(mas[1])
+        if(abs(old_err)) > abs(err):
+            i+= 1
+        else:
+            break
+        old_err = err
+    result = 0
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+    if err != 0:
+        if i < len(distance)-1:
+            if i > 0:
+                if num > distance[i][1]:
+                    x1 = distance[i][1]
+                    x2 = distance[i-1][1]
+                    y1 = distance[i][0]
+                    y2 = distance[i-1][0]
+
+                else:
+                    x2 = distance[i][1]
+                    x1 = distance[i + 1][1]
+                    y1 = distance[i + 1][0]
+                    y2 = distance[i][0]
+                if x2 != x1:
+                    result = ((num - x1)/(x2 - x1))*(y2 - y1) + y1
+                    # result = ((num - x1)/(0.0))*(y2 - y1) + y1
+                else:
+                    result = ((num - x1)/(x2 - x1 + 1))*(y2 - y1) + y1
+                    # result = ((num - x1)/(0.0))*(y2 - y1) + y1
+            else:
+                result = distance[0][0]
+        else:
+          result = distance[-1][0]
+    else:
+        result = distance[i][0]
+    return result
+
+
 
 
 while True:
@@ -255,7 +336,7 @@ while True:
             yellow_x = blob.cx() - center[0]
             yellow_y = blob.cy() - center[1]
             img.draw_rectangle(blob[0], blob[1], blob[2], blob[3],(255,255,0), 2)
-            img.draw_line(center[0], center[1], blob.cx(), blob.cy(), (255,255,0), thickness = 2)
+            #img.draw_line(center[0], center[1], blob.cx(), blob.cy(), (255,255,0), thickness = 2)
             img.draw_circle(blob.cx(), blob.cy(), 3, (255,255,0), fill = True)
             yellow_distance = linearize(get_distance(yellow_x, yellow_y))
             yellow_angle = math.floor(math.atan2(yellow_x, yellow_y) * 57.3)+90
@@ -278,7 +359,7 @@ while True:
             blue_x = blob.cx() - center[0]
             blue_y = blob.cy() - center[1]
             img.draw_rectangle(blob[0], blob[1], blob[2], blob[3],(0,0,255), 2)
-            img.draw_line(center[0], center[1], blob.cx(), blob.cy(), (0,0,255), thickness = 2)
+            #img.draw_line(center[0], center[1], blob.cx(), blob.cy(), (0,0,255), thickness = 2)
             img.draw_circle(blob.cx(), blob.cy(), 3, (0,0,255), fill = True)
             blue_distance = linearize(get_distance(blue_x, blue_y))
             blue_angle = math.floor(math.atan2(blue_x, blue_y) * 57.3)+90
@@ -297,19 +378,19 @@ while True:
     old_roundness = 0
     max_area = 0
     ball_distance = 0
-    for blob in img.find_blobs(red_threshold, pixels_threshold=0, area_threshold=0, merge=True, margin = 10):
-        if blob.area() >= 7 and blob.area() < 350:
-            if blob.compactness() > old_roundness:
-                old_roundness = blob.compactness()
-                my_blob = blob
-                max_area = blob.area()
-                my_blob_x = blob.cx()
-                my_blob_y = blob.cy()
-                ball_x = (my_blob_x - center[0])
-                ball_y = (my_blob_y - center[1])
-                ball_distance = linearize(get_distance(ball_x, ball_y))
-                ball_angle = math.floor(math.atan2(ball_x, ball_y) * 57.3) + 90
-                img.draw_circle(my_blob_x, my_blob_y, 20, (255, 255, 255), 4)
+    ball_angle = 0
+    for blob in img.find_blobs(red_threshold, pixels_threshold=1, area_threshold=0, merge=True, margin = 7):
+    #if blob.compactness() > old_roundness:
+        old_roundness = blob.compactness()
+        my_blob = blob
+        max_area = blob.area()
+        my_blob_x = blob.cx()
+        my_blob_y = blob.cy()
+        ball_x = (my_blob_x - center[0])
+        ball_y = (my_blob_y - center[1])
+        ball_distance = linearize_ll(get_distance(ball_x, ball_y))
+        ball_angle = math.floor(math.atan2(ball_x, ball_y) * 57.3) + 90
+        img.draw_circle(my_blob_x, my_blob_y, 20, (255, 255, 255), 4)
     #print(max_area)
 
     #print("ball_distance =", ball_distance)
